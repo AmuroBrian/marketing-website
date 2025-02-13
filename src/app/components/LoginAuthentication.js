574b046880e1307b0e12208991a5d2bd7735ca98
denseeen <villanueva.194566@cubao.sti.edu.ph>
2025-02-13 15:20:43 +0800

    Message updated

--- src/app/components/LoginAuthentication.js
+++ src/app/components/LoginAuthentication.js
@@ -4,10 +4,8 @@ export default function LoginAuthentication() {
     return (
         <div 
             className="bg-gray-500 mt-[-20] h-[80vh] bg-cover bg-center flex justify-center items-center text-white text-3xl"
-            
         >
             Sir Brian
         </div>
     );
 }
-    
