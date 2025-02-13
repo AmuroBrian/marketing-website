65990134159cc61c2d16d25e52b11cd2a6e3cd0f
denseeen <villanueva.194566@cubao.sti.edu.ph>
2025-02-13 13:53:06 +0800

    Landing page

---
+++ src/app/components/LoginAuthentication.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function LoginAuthentication() {
+    return (
+        <div 
+            className="bg-gray-500 mt-[-20] h-[80vh] bg-cover bg-center flex justify-center items-center text-white text-3xl"
+            
+        >
+            Sir Brian
+        </div>
+    );
+}
+    
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,18 @@
-import Image from "next/image";
+import LoginAuthentication from "./components/LoginAuthentication";
+import Navigation from "./components/navigation"; // Make sure "Navigation" has a capital "N"
 
 export default function Home() {
   return (
-    <div>
-      Start with here
+    <div className="flex flex-col min-h-screen">
+      {/* Top Section - LoginAuthentication */}
+      <div>
+        <LoginAuthentication />
+      </div>
+
+      {/* Bottom Section - Navigation (Sticky to Bottom) */}
+      <div>
+        <Navigation />
+      </div>
     </div>
   );
 }
