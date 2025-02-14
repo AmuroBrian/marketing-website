b982b2175f6d0c7b9f44c83413f3ba2f6d3db3c3
Amuro Brian <perezbrian.sti@gmail.com>
2025-02-14 14:44:33 +0800

    Added Carousel

--- src/app/page.js
+++ src/app/page.js
@@ -4,22 +4,16 @@ import WhatIsAgent from "./components/WhatIsAgent";
 import EmailForm from "./components/EmailForm";
 import Navigation from "./components/navigation";
 
-
-
 export default function Home() {
   return (
-
     <div className="flex flex-col min-h-screen">
       <div>
         <Navigation />
       </div>
-
       <div>
         <WhatIsAgent />
         <EmailForm />
       </div>
-
     </div>
-
   );
 };
